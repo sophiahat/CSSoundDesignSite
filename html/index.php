6bd299be69ea819dc9a7af49ed4501a0c4b1a013
lbs <lbspen@gmail.com>
2013-07-31 17:03:32 -0700

    initial download from server

---
+++ html/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
+<head>
+    <!--
+    Design by Arnaud Valle
+    http://arnaudvalle.free.fr/
+
+    Licensed under the Creative Commons Attribution 3.0 Unported (http://creativecommons.org/licenses/by/3.0/)
+    You are free to copy, distribute, adapt the work, but you must keep a link of some sort to me (in the footer or source).
+
+
+    -->
+    <meta http-equiv="Content-Type"
+          content="text/html; charset=ISO-8859-1">
+    <meta name="description"
+          content="Chris Spencer Creative is an online portfolio of sound design, music composition and sound recordings of musician, audio engineer, composer Chris Spencer">
+    <meta name="keywords" content="sound design,
+  Seattle sound design,
+  Seattle sound designer,
+  Seattle sound editing,
+  Seattle voice over recording,
+  Seattle ADR,
+  Seattle film sound,
+  Seattle dialog replacement,
+  Seattle sound effects,
+  Seattle game audio,
+  seattle game sound,
+  Seattle sound efx,
+  seattle sound for games,
+  Seattle,
+  Pacific Northwest,
+  sound design pacific northwest,
+  pacific northwest sound design,
+  sound editing,
+  voice over recording,
+  ADR,
+  film sound,
+  dialog replacement,
+  sound effects,
+  sound EFX,
+  Chris Spencer,
+  musician,
+  sound recorder,
+  recording studio,
+  game audio,
+  music composition,
+  soundtrack,
+  film scoring,
+  Website sound,
+  web sound,
+  Web audio,
+  website audio,
+  website music,
+  website soundtrack,
+  project studio,
+  sound for games">
+    <meta name="author" content="author">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Chris Spencer Creative - Solutions in Audio</title>
+
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-20609405-2']);
+        _gaq.push(['_trackPageview']);
+
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+</head>
+<body>
+<div id="container">
+    <div id="header">
+        <h1><span>Chris Spencer Creative</span> Sound Design<br>
+        </h1>
+    </div>
+    <div id="sidebar">
+        <?php
+        include 'common/nav.html';
+        include 'common/shows.html';
+        ?>
+    </div>
+    <div id="main">
+        <div class="divider"></div>
+        <br>
+
+        <h2><big><big><big><big>Innovative original Audio content for
+            Interactive Games, the Web, Radio, TV and Film</big></big></big></big></h2>
+
+        <div class="divider"></div>
+        <br>
+
+        <h2>Home</h2>
+        <br>
+        ChrisSpencerCreative is my online portfolio of sound design,
+        compositions and music. It is a showcase of my work and illustrates my
+        capability to handle all of your sound design needs.<br>
+        <br>
+
+        <h2>Professional Sound Design</h2>
+        <br>
+        I provide the perfect finish for your video game, audio book, TV/radio
+        spot, web-based content and film/video project with voice-overs, ADR,
+        audio sweetening, live instrument tracking, sound effects, music
+        composition, and audio mixing and mastering.<br>
+        <br>
+        <a href=""><br>
+        </a>
+
+        <div class="divider"></div>
+        <h2>My Credo...</h2>
+        <br>
+        You will receive not only a professional product backed by extensive
+        experience, but a creative, detailed sound experience in a timely and
+        affordable manner.
+
+        <div class="divider"></div>
+        <br>
+    </div>
+    <?php include 'common/footer.html'; ?>
+</div>
+</body>
+</html>
