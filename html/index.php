b97c82c406e50275ba934e367a5ee065e81d3ded
lbs <lbspen@gmail.com>
2013-08-01 23:53:04 -0700

    add css class for subtitle

--- html/index.php
+++ html/index.php
@@ -1,69 +1,69 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<meta name="description" content="Chris Spencer Creative is an online portfolio of sound design, music composition and sound recordings of musician, audio engineer, composer Chris Spencer">
-<meta name="keywords" content="sound design,
-Seattle sound design,
-Seattle sound designer,
-Seattle sound editing,
-Seattle voice over recording,
-Seattle ADR,
-Seattle film sound,
-Seattle dialog replacement,
-Seattle sound effects,
-Seattle game audio,
-seattle game sound,
-Seattle sound efx,
-seattle sound for games,
-Seattle,
-Pacific Northwest,
-sound design pacific northwest,
-pacific northwest sound design,
-sound editing,
-voice over recording,
-ADR,
-film sound,
-dialog replacement,
-sound effects,
-sound EFX,
-Chris Spencer,
-musician,
-sound recorder,
-recording studio,
-game audio,
-music composition,
-soundtrack,
-film scoring,
-Website sound,
-web sound,
-Web audio,
-website audio,
-website music,
-website soundtrack,
-project studio,
-sound for games">
-<meta name="author" content="author">
-<link rel="stylesheet" href="css/meyer_reset.css" type="text/css">
-<link rel="stylesheet" href="css/style.css" type="text/css">
-<title>Chris Spencer Creative - Solutions in Audio</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <meta name="description" content="Chris Spencer Creative is an online portfolio of sound design, music composition and sound recordings of musician, audio engineer, composer Chris Spencer">
+  <meta name="keywords" content="sound design,
+  Seattle sound design,
+  Seattle sound designer,
+  Seattle sound editing,
+  Seattle voice over recording,
+  Seattle ADR,
+  Seattle film sound,
+  Seattle dialog replacement,
+  Seattle sound effects,
+  Seattle game audio,
+  seattle game sound,
+  Seattle sound efx,
+  seattle sound for games,
+  Seattle,
+  Pacific Northwest,
+  sound design pacific northwest,
+  pacific northwest sound design,
+  sound editing,
+  voice over recording,
+  ADR,
+  film sound,
+  dialog replacement,
+  sound effects,
+  sound EFX,
+  Chris Spencer,
+  musician,
+  sound recorder,
+  recording studio,
+  game audio,
+  music composition,
+  soundtrack,
+  film scoring,
+  Website sound,
+  web sound,
+  Web audio,
+  website audio,
+  website music,
+  website soundtrack,
+  project studio,
+  sound for games">
+  <meta name="author" content="author">
+  <link rel="stylesheet" href="css/meyer_reset.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <title>Chris Spencer Creative - Solutions in Audio</title>
 
-<script type="text/javascript">
+  <script type="text/javascript">
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-20609405-2']);
-_gaq.push(['_trackPageview']);
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20609405-2']);
+  _gaq.push(['_trackPageview']);
 
-(function () {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
+  (function () {
+   var ga = document.createElement('script');
+   ga.type = 'text/javascript';
+   ga.async = true;
+   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(ga, s);
+ })();
 
-</script>
+ </script>
 </head>
 <body>
 	<div id="container">
@@ -78,7 +78,7 @@ _gaq.push(['_trackPageview']);
 		</div>
 		<div id="main">
 			<div class="divider"></div>
-			<h4>Innovative original Audio content for Interactive Games, the Web, Radio, TV and Film</h4>
+			<div class="subtitle">Innovative original Audio content for Interactive Games, the Web, Radio, TV and Film</div>
       <div class="divider"></div>
       <h2>Home</h2>
       <p>
--- html/portfolio.php
+++ html/portfolio.php
@@ -32,52 +32,63 @@
       <iframe src="http://player.vimeo.com/hubnut/group/201076?color=b6da6e&amp;background=000000&amp;slideshow=1&amp;video_title=1&amp;video_byline=1"frameborder="0"webkitAllowFullScreen mozallowfullscreen allowFullScreen>
       </iframe>
       <h2>Production Notes</h2>
-      <h3>Dead Space</h3> <h4>Work Performed: Score and Sound Design</h4> 
+      <h3>Dead Space</h3> 
+      <div class="subtitle">Work Performed: Score and Sound Design</div> 
       <p>
         Here is an example of a current game trailer.  The mood is rather dark as it opens as a dream sequence.  There is a large amount of sci-fi and spacey effects in the intro, followed by an operating room environment and finally a few zombie types.  I used water droplets for a lot of the small object chatter that occurs before the rock tune and near the end of the sequence.  Also of note, I use velcro to create the trail of cracking that connects the zombies.
         Most of the music was done in Logic, and the effects and mix were done in Pro Tools. 
       </p>
-      <h2>Aflac</h2> Work Performed: Score, Sound Design and Voice Over
+      <h3>Aflac</h3> 
+      <div class="subtitle">Work Performed: Score, Sound Design and Voice Over</div>
       <p>
         This piece illustrates a specific genre in sound design and scoring that I love and wanted to explore.  
         I tried to pay homage to the original scoring philosophy on those old Warner Brothers cartoons. I used traditional orchestral sounds from Logic 8 throughout (except for the electric guitar at the very end) with the harmonic and rhythmic motion exaggerating the hit points.  The sounds that I chose for impacts were also in remembrance of those funny over-the-top sonic nuggets found in those cartoons, as they were often the punchline for the joke.  I ended up pitching up most of the voice over to give it that cartoony flavor; we had a lot of fun trying to conjure up a performance from the actors.  
       </p>
-      <h2>BreachAndClear</h2> Work Performed: Score &amp; Sound Design 
+      <h3>BreachAndClear</h3>
+      <div class="subtitle">Work Performed: Score &amp; Sound Design</div> 
       <p>
         This is a game trailer for <a href="http://www.fearthegun.com/">Gun Studios</a>.  I did the score in Logic using Kontakt instruments and the sound design and mix in Pro Tools.  
       </p>
-      <h2>Sam</h2> Work Performed: Sound Design, Music Editing 
+      <h3>Sam</h3>
+      <div class="subtitle">Work Performed: Sound Design, Music Editing</div> 
       <p>
         This piece showcases both my sound design skills and music editing chops. 
         I did all the sound design in this project and used an existing score from a famous composer to illustrate my abilities of making existing music fit the film.  The soundtrack gives the flick a dark tone, which I think compliments the story nicely.  I did all the work in protools except for the score edit which was done in Audition.
       </p> 
-      <h2>Facing Worlds</h2> Work Performed:  replacement of all ambiences, weapon pickups and discharges
+      <h3>Facing Worlds</h3>
+      <div class="subtitle">Work Performed:  replacement of all ambiences, weapon pickups and discharges</div>
       <p>
         This piece illustrates my ability to integrate sound design using the Unreal Editor.
         I took an existing level and replaced the static sounds as well as recreated weapons pickups and discharges.  I added more ambient and static sounds that were previously there, such as the serpent sounds made from stretching rubber, handling a gel pack and krinkling bubble wrap.  The weapons pickups and discharges were mostly composites using real weapons fire with other unrelated sounds. 
       </p>
-      <h2>Efile</h2> Work Performed: All sound elements including narration and incidental VO
+      <h3>Efile</h3> 
+      <div class="subtitle">Work Performed: All sound elements including narration and incidental VO</div>
       <p>
         This is a funny little commercial about a man, his tax return and a mailbox.  I exaggerated the interaction between the man and the mailbox with the impact sounds to add to the humor.  This project was done exclusively in Pro Tools.
       </p>
-      <h2>Painting Day</h2> Work Performed: All sound design elements and music composition
+      <h3>Painting Day</h3> 
+      <div class="subtitle">Work Performed: All sound design elements and music composition</div>
       <p>
         Here's another project that was partially done in Logic (soundtrack) and in Pro tools (sound design and final mix).  I wanted to recreate a slapstick type audioscape that emphasized a fairly large portion of the movement in this piece.  So, there is a lot of synchronization between the action, score and sound effects.  I used an acoustic guitar as well as virtual  guitar samples combined to create the main instrument for the score. 
       </p> 
-      <h2>Fancy Nancy</h2> Work Performed: All sound design, score and voice over
+      <h3>Fancy Nancy</h3> 
+      <div class="subtitle">Work Performed: All sound design, score and voice over</div>
       <p>
         This piece is a unique twist on the audio book format with the use of a child narrator and a video representation of the actual book. 
         I used my daughter as the narrator and I comped the performance together using Pro tools.  The sound design and music was then added within the same session. Each page of the book was scanned, sent to iPhoto for editing, and finally sent to iMovie to be united with the audio.
       </p> 
-      <h2>Epsilon 3</h2> Work performed: joint composition, score mix
+      <h3>Epsilon 3</h3> 
+      <div class="subtitle">Work performed: joint composition, score mix</div>
       <p>
         On this project I contributed an original score to go along with a full length film created by filmmaker Steve Rudolf.  Using a trio of guitar, bass and drums, all with lots of effects, we tracked most of this live, playing along with the edited film.  The scenes that we contributed to were mostly open ended exploration type sequences, so it freed us up quite a bit.  All work was performed in Pro tools on this one.  
       </p>
-      <h2>Century 21</h2> Work Performed:  All sound design and dialog
+      <h3>Century 21</h3> 
+      <div class="subtitle">Work Performed:  All sound design and dialog</div>
       <p>
         On this one I replaced the original dialogue with different actors and created all the sound design using Pro Tools.  
       </p>
-      <h2>Stevinson</h2> Work Performed: All sound design and dialog, music editing
+      <h3>Stevinson</h3>
+      <div class="subtitle">Work Performed: All sound design and dialog, music editing</div>
       <p>
         This was a funny commercial about a man and his best friend who happened to be a cardboard representation of his local car dealer.  I chose a comical swing tune from a library to be soundtrack, replaced all the dialog and added all the ambience and effects.  
       </p>
